f59b7541653b301d3126395e121f50d4bc2fce77
Flo <greendoors@gmail.com>
2019-03-02 08:43:59 +0000

    Update retrieve_IUCN_data.R

--- retrieve_IUCN_data.R
+++ retrieve_IUCN_data.R
@@ -5,8 +5,10 @@
 library(rredlist)
 library(tidyverse)
 
-# Fuction
-retrieve_IUCN_status <- function(speciesList){
+#####################################
+# FUNCTION
+
+retrieve_IUCN_data <- function(speciesList){
   IUCN_status <- data.frame(Species = character(), Status = character(), 
                             Trend = character(), stringsAsFactors=FALSE)
   for(sp in speciesList){
@@ -30,12 +32,45 @@ retrieve_IUCN_status <- function(speciesList){
   return(IUCN_status)
 }
 
+#####################################
+# RUN
+
 # 1) Species List
-canids <- c('Cerdocyon thous', 'Lycalopex gymnocercus', 'Chrysocyon brachyurus')
-# Run
-retrieve_IUCN_status(canids)
+canids <- c('Cerdocyon thous', 'Lycalopex gymnocercus', 'Chrysocyon brachyurus', 'Wrong species')
+canids_IUCN_data <- retrieve_IUCN_data(canids)
+
+## The fuction will print in console
+# Cerdocyon thous LC Stable 
+# Lycalopex gymnocercus LC Stable 
+# Chrysocyon brachyurus NT Unknown 
+# Wrong species ------ CHECK
 
 # 2) Data from table
 mammals <- read_csv('speciesList.csv')
-# Run
-retrieve_IUCN_status(mammals$Species)
+mammlas_IUCN_data <- retrieve_IUCN_data(mammals$Species)
+
+mammlas_IUCN_data
+#                      Species Status      Trend
+# 1         Mazama gouazoubira     LC Decreasing
+# 2                  Axis axis     LC    Unknown
+# 3                 Sus scrofa     LC    Unknown
+# 4            Cerdocyon thous     LC     Stable
+# 5      Chrysocyon brachyurus     NT    Unknown
+# 6      Lycalopex gymnocercus     LC     Stable
+# 7           Leopardus wiedii     NT Decreasing
+# 8        Leopardus braccatus     NA         NA
+# 9        Leopardus geoffroyi     LC     Stable
+# 10         Puma yagouaroundi     NA         NA
+# 11        Lontra longicaudis     NT Decreasing
+# 12          Conepatus chinga     LC Decreasing
+# 13             Galictis cuja     LC    Unknown
+# 14       Procyon cancrivorus     LC Decreasing
+# 15      Dasypus novemcinctus     LC     Stable
+# 16     Euphractus sexcinctus     LC     Stable
+# 17         Cabassous tatouay     LC    Unknown
+# 18     Didelphis albiventris     LC     Stable
+# 19           Lepus europaeus     LC Decreasing
+# 20            Cuniculus paca     LC     Stable
+# 21          Coendou spinosus     LC    Unknown
+# 22 Hydrochoerus hydrochaeris     LC     Stable
+# 23     Tamandua tetradactyla     LC    Unknown
