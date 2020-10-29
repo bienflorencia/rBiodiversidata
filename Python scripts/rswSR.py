a4e51f21e6a9bc1bf304b474b5f0abf1e6a205d1
Flo <greendoors@gmail.com>
2020-10-29 13:00:26 +0000

    Update rswSR.py

--- Python scripts/rswSR.py
+++ Python scripts/rswSR.py
@@ -7,17 +7,16 @@ Created on Wed Jun 19 16:43:48 2019
 
 import csv
             
-
 # FUNCTIONS  
 
-def dic_species_fraction_per_grid(arch):
+def dic_species_fraction_per_grid(archivo):
     dic_species_fraction = {}
     with open(archivo) as csvfile:
         reader = csv.reader(csvfile)
         next(reader)
         for row in reader:
             especie = row[sp_row].split(';')
-            sp_count = row[join_count]
+            sp_count = row[N_row]
             lista_especies_por_celda = []
             if sp_count != '0':
                 for sp in especie:
@@ -39,16 +38,19 @@ def rswSR_calculation(archivo, dic_freq):
         for row in reader:
             rswSR = 0
             especie = row[sp_row].split(';')
-            sp_count = row[join_count]
-            id_row = row[0]
+            sp_count = row[N_row]
+            id_row = row[grid_row]
             if sp_count == '0':
                 dic_rswSR_calculation[id_row]=0
             else:
                 rswSR = 0
+                lista_especies_por_celda = []
                 for sp in especie:
                     if sp in dic_freq:
-                        rswSR = rswSR + (1/dic_freq[sp])
-                        dic_rswSR_calculation[id_row]=rswSR
+                        if sp not in lista_especies_por_celda:
+                            rswSR = rswSR + (1/dic_freq[sp])
+                            dic_rswSR_calculation[id_row]=rswSR
+                            lista_especies_por_celda.append(sp)
     return dic_rswSR_calculation
 
   
@@ -56,12 +58,13 @@ def rswSR_calculation(archivo, dic_freq):
 
 # FILES     
     
-archivo = 'Tetrapods_UY125.csv'
+archivo = 'Grid_UY125_Lycophytes.csv'
 name_archivo = archivo.split('.')[0]
 new_archivo = name_archivo+'_rswSR_calculated.csv'
 
-sp_row=4
-join_count=1
+grid_row=0
+sp_row=3
+N_row=1
         
 # RUN   
 
@@ -73,5 +76,4 @@ with open(new_archivo, 'w', newline='', encoding="utf8") as ofile:
     writer.writerow(['ID', 'rswSR'])
     for key in rswSR.keys():
         writer.writerow([key, rswSR[key]])
-        
         
