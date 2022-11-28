156aa804409476854f4ac28de65d3b5dac72acc7
Florencia Grattarola <flograttarola@gmail.com>
2022-11-28 15:38:11 +0100

    Update check_species_names.R
    updated version to include previousIdentification

--- Data Cleaning and Standardisation Scripts/check_species_names.R
+++ Data Cleaning and Standardisation Scripts/check_species_names.R
@@ -9,7 +9,7 @@ library(tidyverse)
 
 # TAXONOMIC DATA SOURCES
 sources <- gnr_datasources()
-itis <- sources$id[sources$title == 'ITIS']
+itis <- sources$id[sources$title == 'Integrated Taxonomic Information SystemITIS']
 iucn <- sources$id[sources$title == 'IUCN Red List of Threatened Species']
 
 #####################################
@@ -17,41 +17,47 @@ iucn <- sources$id[sources$title == 'IUCN Red List of Threatened Species']
 # FUNCTION
 
 check_name <- function(species){
-  species_checked <- data.frame(Species = character(),
-                                Observation = character(), 
+  species_checked <- data.frame(previousIdentification = character(),
+                                scientificName = character(),
+                                observation = character(), 
                                 stringsAsFactors=FALSE)
   for(sp in species) {
     sp_check_itis <- gnr_resolve(sp, data_source_ids=itis, with_canonical_ranks = TRUE)
-    if (length(sp_check_itis)==0){
+    if (nrow(sp_check_itis)==0){
       sp_check_iucn <- gnr_resolve(sp, data_source_ids=iucn, with_canonical_ranks = TRUE)
-      if (length(sp_check_iucn)==0){
-        sp_checked <- data.frame(Species = sp,
-                                 Observation = 'NOT FOUND in ITIS or IUCN', 
+      if (nrow(sp_check_iucn)==0){
+        sp_checked <- data.frame(previousIdentification = sp,
+                                 scientificName = NA,
+                                 observation = 'NOT FOUND in ITIS or IUCN', 
                                  stringsAsFactors=FALSE)
         species_checked <- rbind(species_checked, sp_checked)
       }
-      else if (length(sp_check_iucn)!=0 && sp!=sp_check_iucn$matched_name2){
-        sp_checked <- data.frame(Species = sp_check_iucn$matched_name2,
-                                 Observation = 'Checked IUCN', 
+      else if (nrow(sp_check_iucn)!=0 && sp!=sp_check_iucn$matched_name2){
+        sp_checked <- data.frame(previousIdentification = sp,
+                                 scientificName = sp_check_iucn$matched_name2,
+                                 observation = 'Checked IUCN', 
                                  stringsAsFactors=FALSE)
         species_checked <- rbind(species_checked, sp_checked)
       }
       else{
-        sp_checked <- data.frame(Species = sp,
-                                 Observation = 'Ok IUCN', 
+        sp_checked <- data.frame(previousIdentification = sp,
+                                 scientificName = sp,
+                                 observation = 'Ok IUCN', 
                                  stringsAsFactors=FALSE)
         species_checked <- rbind(species_checked, sp_checked)
       }
     } 
-    else if (length(sp_check_itis)!=0 && sp!=sp_check_itis$matched_name2){
-      sp_checked <- data.frame(Species = sp_check_itis$matched_name2,
-                               Observation = 'Checked ITIS', 
+    else if (nrow(sp_check_itis)!=0 && sp!=sp_check_itis$matched_name2){
+      sp_checked <- data.frame(previousIdentification = sp,
+                               scientificName = sp_check_itis$matched_name2,
+                               observation = 'Checked ITIS', 
                                stringsAsFactors=FALSE)
       species_checked <- rbind(species_checked, sp_checked)
     } 
     else {
-      sp_checked <- data.frame(Species = sp,
-                               Observation = 'Ok ITIS', 
+      sp_checked <- data.frame(previousIdentification = sp,
+                               scientificName = sp,
+                               observation = 'Ok ITIS', 
                                stringsAsFactors=FALSE)
       species_checked <- rbind(species_checked, sp_checked)
     }
@@ -63,40 +69,41 @@ check_name <- function(species){
 # RUN
 
 # 1) Species List
-canids <- c('Cerdocyon thous', 'Lycalopex gymnocercus', 'Chrysocyon brachyurus', 'Wrong species')
+canids <- c('Cerdocyon thous', 'Cerdocyon thou', 'Aerdocyon thou')
 canids_species_check <- check_name(canids)
+canids_species_check
 
-#                 Species               Observation
-# 1       Cerdocyon thous                   Ok ITIS
-# 2 Lycalopex gymnocercus                   Ok ITIS
-# 3 Chrysocyon brachyurus                   Ok ITIS
-# 4         Wrong species NOT FOUND in ITIS or IUCN
+#   previousIdentification  scientificName  observation
+# 1        Cerdocyon thous Cerdocyon thous      Ok ITIS
+# 2         Cerdocyon thou Cerdocyon thous Checked ITIS
+# 3         Aerdocyon thou Cerdocyon thous Checked ITIS
 
 # 2) Data from table
-mammals <- read_csv('speciesList.csv')
+mammals <- read_csv('tetrapodsSpeciesList.csv')
 mammlas_species_check <- check_name(mammals$Species)
+mammlas_species_check
 
-#                      Species               Observation
-# 1         Mazama gouazoubira                   Ok ITIS
-# 2                  Axis axis                   Ok ITIS
-# 3                 Sus scrofa                   Ok ITIS
-# 4            Cerdocyon thous                   Ok ITIS
-# 5      Chrysocyon brachyurus                   Ok ITIS
-# 6      Lycalopex gymnocercus                   Ok ITIS
-# 7           Leopardus wiedii                   Ok ITIS
-# 8        Leopardus braccatus                   Ok ITIS
-# 9        Leopardus geoffroyi                   Ok ITIS
-# 10         Puma yagouaroundi                   Ok ITIS
-# 11        Lontra longicaudis                   Ok ITIS
-# 12          Conepatus chinga                   Ok ITIS
-# 13             Galictis cuja                   Ok ITIS
-# 14       Procyon cancrivorus                   Ok ITIS
-# 15      Dasypus novemcinctus                   Ok ITIS
-# 16     Euphractus sexcinctus                   Ok ITIS
-# 17         Cabassous tatouay                   Ok ITIS
-# 18     Didelphis albiventris                   Ok ITIS
-# 19           Lepus europaeus                   Ok ITIS
-# 20            Cuniculus paca                   Ok ITIS
-# 21                No species NOT FOUND in ITIS or IUCN
-# 22 Hydrochoerus hydrochaeris                   Ok ITIS
-# 23     Tamandua tetradactyla                   Ok ITIS
+#       previousIdentification            scientificName               observation
+# 1         Mazama gouazoubira        Mazama gouazoubira                   Ok ITIS
+# 2                  Axis axis                 Axis axis                   Ok ITIS
+# 3                 Sus scrofa                Sus scrofa                   Ok ITIS
+# 4            Cerdocyon thous           Cerdocyon thous                   Ok ITIS
+# 5      Chrysocyon brachyurus     Chrysocyon brachyurus                   Ok ITIS
+# 6      Lycalopex gymnocercus     Lycalopex gymnocercus                   Ok ITIS
+# 7           Leopardus wiedii          Leopardus wiedii                   Ok ITIS
+# 8        Leopardus braccatus       Leopardus braccatus                   Ok ITIS
+# 9        Leopardus geoffroyi       Leopardus geoffroyi                   Ok ITIS
+# 10         Puma yagouaroundi         Puma yagouaroundi                   Ok ITIS
+# 11        Lontra longicaudis        Lontra longicaudis                   Ok ITIS
+# 12          Conepatus chinga          Conepatus chinga                   Ok ITIS
+# 13             Galictis cuja             Galictis cuja                   Ok ITIS
+# 14       Procyon cancrivorus       Procyon cancrivorus                   Ok ITIS
+# 15      Dasypus novemcinctus      Dasypus novemcinctus                   Ok ITIS
+# 16     Euphractus sexcinctus     Euphractus sexcinctus                   Ok ITIS
+# 17         Cabassous tatouay         Cabassous tatouay                   Ok ITIS
+# 18     Didelphis albiventris     Didelphis albiventris                   Ok ITIS
+# 19           Lepus europaeus           Lepus europaeus                   Ok ITIS
+# 20            Cuniculus paca            Cuniculus paca                   Ok ITIS
+# 21                No species                      <NA> NOT FOUND in ITIS or IUCN
+# 22 Hydrochoerus hydrochaeris Hydrochoerus hydrochaeris                   Ok ITIS
+# 23     Tamandua tetradactyla     Tamandua tetradactyla                   Ok ITIS
