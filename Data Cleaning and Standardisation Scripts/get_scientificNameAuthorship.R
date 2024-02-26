383e828cba60e51f011493b876f8231de1d16cf7
Florencia Grattarola <flograttarola@gmail.com>
2024-02-26 14:16:15 +0100

    Update get_scientificNameAuthorship.R
    I edited the function to match gnr_sources updates and to include the source as an argument in the function. I also updated the path to the csv file with the list of species.

--- Data Cleaning and Standardisation Scripts/get_scientificNameAuthorship.R
+++ Data Cleaning and Standardisation Scripts/get_scientificNameAuthorship.R
@@ -6,82 +6,80 @@ library(taxize)
 library(tidyverse)
 
 sources <- gnr_datasources()
-itis <- sources$id[sources$title == 'ITIS']
-col <- sources$id[sources$title == 'Catalogue of Life']
-
+itis <- sources %>% filter(grepl('ITIS', title)) %>% pull(id)
+col <- sources %>% filter(grepl('Catalogue of Life', title)) %>% pull(id)
 
 #####################################
 
 # FUNCTION
 
-get_scientificNameAuthorship <- function(species){
-  species_scientificNameAuthorship <- data.frame(Species = character(),
-                                                 Authorship = character(),
-                                                 stringsAsFactors=FALSE)
+get_scientificNameAuthorship <- function(species, data_source){
+  species_scientificNameAuthorship <- tibble(scientificName = character(),
+                                             scientificNameAuthorship = character())
   for(sp in species) {
-    sp_search <- gnr_resolve(sp, data_source_ids=itis, best_match_only=TRUE)
-    if (length(sp_search)==0){
-      sp_Authorship <- data.frame(Species = sp,
-                                  Authorship = 'NOT FOUND',
-                                  stringsAsFactors=FALSE)
+    sp_search <- gnr_resolve(sci=sp, data_source_ids=data_source, best_match_only=TRUE)
+    if (nrow(sp_search)==0){
+      sp_Authorship <- tibble(scientificName = sp,
+                              scientificNameAuthorship = 'NOT FOUND')
       species_scientificNameAuthorship <- rbind(species_scientificNameAuthorship, sp_Authorship)
     }
     else if (sp_search$score <= 0.9){
-      sp_Authorship <- data.frame(Species = sp,
-                                  Authorship = 'NOT FOUND',
-                                  stringsAsFactors=FALSE)
+      sp_Authorship <- tibble(scientificName = sp,
+                              scientificNameAuthorship = 'NOT FOUND')
       species_scientificNameAuthorship <- rbind(species_scientificNameAuthorship, sp_Authorship)
     }
     else {
-      sp_Authorship <- data.frame(Species = sp,
-                                  Authorship = sp_search$matched_name,
-                                  stringsAsFactors=FALSE)
+      sp_Authorship <- tibble(scientificName = sp,
+                              scientificNameAuthorship = sp_search$matched_name)
       species_scientificNameAuthorship <- rbind(species_scientificNameAuthorship, sp_Authorship)
     }
-    
   }
   return(species_scientificNameAuthorship)
 }
-
 
 #####################################
 # RUN
 
 # 1) Species List
 canids <- c('Cerdocyon thous', 'Lycalopex gymnocercus', 'Chrysocyon brachyurus', 'Wrong species')
-get_scientificNameAuthorship(canids)
+get_scientificNameAuthorship(canids, itis)
 
-#                 Species                               Authorship
-# 1       Cerdocyon thous         Cerdocyon thous (Linnaeus, 1766)
-# 2 Lycalopex gymnocercus Lycalopex gymnocercus (G. Fischer, 1814)
-# 3 Chrysocyon brachyurus    Chrysocyon brachyurus (Illiger, 1815)
-# 4         Wrong species                                NOT FOUND
+# # A tibble: 4 × 2
+#   scientificName          scientificNameAuthorship                
+#   <chr>                   <chr>                                   
+#   1 Cerdocyon thous       Cerdocyon thous (Linnaeus, 1766)        
+#   2 Lycalopex gymnocercus Lycalopex gymnocercus (G. Fischer, 1814)
+#   3 Chrysocyon brachyurus Chrysocyon brachyurus (Illiger, 1815)   
+#   4 Wrong species         NOT FOUND 
 
 # 2) Data from table
-mammals <- read_csv('speciesList.csv')
-get_scientificNameAuthorship(mammals$Species)
+mammals <- read_csv('tetrapodsSpeciesList.csv')
+get_scientificNameAuthorship(mammals$Species, itis)
 
-#                      Species                                           Authorship
-# 1         Mazama gouazoubira Mazama gouazoubira (G. Fischer [von Waldheim], 1814)
-# 2                  Axis axis                           Axis axis (Erxleben, 1777)
-# 3                 Sus scrofa                            Sus scrofa Linnaeus, 1758
-# 4            Cerdocyon thous                     Cerdocyon thous (Linnaeus, 1766)
-# 5      Chrysocyon brachyurus                Chrysocyon brachyurus (Illiger, 1815)
-# 6      Lycalopex gymnocercus             Lycalopex gymnocercus (G. Fischer, 1814)
-# 7           Leopardus wiedii                      Leopardus wiedii (Schinz, 1821)
-# 8        Leopardus braccatus                     Leopardus braccatus (Cope, 1889)
-# 9        Leopardus geoffroyi    Leopardus geoffroyi (d'Orbigny and Gervais, 1844)
-# 10         Puma yagouaroundi  Puma yagouaroundi (É. Geoffroy Saint-Hilaire, 1803)
-# 11        Lontra longicaudis                    Lontra longicaudis (Olfers, 1818)
-# 12          Conepatus chinga                      Conepatus chinga (Molina, 1782)
-# 13             Galictis cuja                         Galictis cuja (Molina, 1782)
-# 14       Procyon cancrivorus         Procyon cancrivorus (G.[Baron] Cuvier, 1798)
-# 15      Dasypus novemcinctus                  Dasypus novemcinctus Linnaeus, 1758
-# 16     Euphractus sexcinctus               Euphractus sexcinctus (Linnaeus, 1758)
-# 17         Cabassous tatouay                  Cabassous tatouay (Desmarest, 1804)
-# 18     Didelphis albiventris                     Didelphis albiventris Lund, 1840
-# 19           Lepus europaeus                         Lepus europaeus Pallas, 1778
-# 20            Cuniculus paca                      Cuniculus paca (Linnaeus, 1766)
-# 21                No species                                            NOT FOUND
-# 22 Hydrochoerus hydrochaeris           Hydrochoerus hydrochaeris (Linnaeus, 1766)
-# 23     Tamandua tetradactyla               Tamandua tetradactyla (Linnaeus, 1758)
+# # A tibble: 23 × 2
+#   scientificName                scientificNameAuthorship                            
+#   <chr>                         <chr>                                               
+#   1 Mazama gouazoubira          Mazama gouazoubira (G. Fischer [von Waldheim], 1814)
+#   2 Axis axis                   Axis axis (Erxleben, 1777)                          
+#   3 Sus scrofa                  Sus scrofa Linnaeus, 1758                           
+#   4 Cerdocyon thous             Cerdocyon thous (Linnaeus, 1766)                    
+#   5 Chrysocyon brachyurus       Chrysocyon brachyurus (Illiger, 1815)               
+#   6 Lycalopex gymnocercus       Lycalopex gymnocercus (G. Fischer, 1814)            
+#   7 Leopardus wiedii            Leopardus wiedii (Schinz, 1821)                     
+#   8 Leopardus braccatus         Leopardus braccatus (Cope, 1889)                    
+#   9 Leopardus geoffroyi         Leopardus geoffroyi (d'Orbigny and Gervais, 1844)   
+#   10 Puma yagouaroundi          Puma yagouaroundi (É. Geoffroy Saint-Hilaire, 1803) 
+#   10 Puma yagouaroundi          Puma yagouaroundi (É. Geoffroy Saint-Hilaire, 1803)
+#   11 Lontra longicaudis         Lontra longicaudis (Olfers, 1818)
+#   12 Conepatus chinga           Conepatus chinga (Molina, 1782)
+#   13 Galictis cuja              Galictis cuja (Molina, 1782)
+#   14 Procyon cancrivorus        Procyon cancrivorus (G.[Baron] Cuvier, 1798)
+#   15 Dasypus novemcinctus       Dasypus novemcinctus Linnaeus, 1758
+#   16 Euphractus sexcinctus      Euphractus sexcinctus (Linnaeus, 1758)
+#   17 Cabassous tatouay          Cabassous tatouay (Desmarest, 1804)
+#   18 Didelphis albiventris      Didelphis albiventris Lund, 1840
+#   19 Lepus europaeus            Lepus europaeus Pallas, 1778
+#   20 Cuniculus paca             Cuniculus paca (Linnaeus, 1766)
+#   21 No species                 NOT FOUND
+#   22 Hydrochoerus hydrochaeris  Hydrochoerus hydrochaeris (Linnaeus, 1766)
+#   23 Tamandua tetradactyla      Tamandua tetradactyla (Linnaeus, 1758)
